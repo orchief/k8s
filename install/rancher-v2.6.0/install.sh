285d9d252e548bf7725642fbb3777b54da31c49d
orchief <dongpeng777777@gmail.com>
2021-09-29 15:49:28 +0000

    添加ranher和istro相关

---
+++ install/rancher-v2.6.0/install.sh
@@ -0,0 +1,5 @@
+# 1. 安装见官方文档 https://docs.rancher.cn/docs/rancher2/quick-start-guide/deployment/quickstart-manual-setup/_index/
+sudo docker run -d --restart=unless-stopped -p 80:80 -p 443:443 rancher/rancher
+# 2. 添加已经搭建好的集群
+
+# 3. 目前不支持汉化补丁 后续等待升级
